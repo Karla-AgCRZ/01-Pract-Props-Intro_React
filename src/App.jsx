1d335c207cebb505434655997ccc0b2cfe4ec178
Karla CRZ <lar.crz.kl@gmail.com>
2025-11-13 15:35:21 -0600

    📝 Ejemplo de procedimiento para trabajar con props por medio de main.jsx y App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function App(props) {
   const [count, setCount] = useState(0)
 
   return (
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      {/* * Importante: ESTÁ LÍNEA DE CÓDIGO EXPLICA DE QUE FORMA SE USARÁ EL PROP */}
+      <h1>{props.saludo} {props.mensaje}</h1> 
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App saludo= 'Hola' mensaje='Muchas gracias a todos, soy un Prop'/>
+    <App saludo= 'Bonjour' mensaje='Merci beaucoup a tous, je suis a Prop'/>   --> ESTE ES UN EJEMPLO DEL USO DE PROPS. */}
+    <App saludo= 'Bonjour' mensaje='Merci beaucoup a tous, je suis a Prop'/>
   </StrictMode>,
 )
